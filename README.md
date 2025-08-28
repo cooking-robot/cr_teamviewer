938a8525c617a44b800c88a6d3bb12259c6f96f0
Remi BONNET <bonnet@fanaticalhelp.com>
2025-08-28 11:15:23 +0200

    open the world

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# cr_teamviewer
+# TeamViewer
 
-Install and configure TeamViewer,
+Install and configure TeamViewer.
 
 This project is a part of Cooking Robot group.
 
+Warning: not tested on Mac OS X.
+
+Now, it only install the full teamviewer client.
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook:: cr_teamviewer
 # Recipe:: default
 #
-# Copyright:: 2025, The Authors, All Rights Reserved.
+# Copyright:: 2025, Remi BONNET, GPL v3.
 
 if platform_family?('mac_os_x')
   tmp_file = "#{Chef::Config['file_cache_path']}/TeamViewer.dmg"
